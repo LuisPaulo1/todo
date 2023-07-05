e58b788c619a8deeba109fdb5fd040524d6d2c26
LuisPaulo1 <csluispaulo@gmail.com>
2023-07-05 17:32:45 -0300

    Primeiro commit

---
+++ backend/src/test/java/com/dominio/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominio.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
