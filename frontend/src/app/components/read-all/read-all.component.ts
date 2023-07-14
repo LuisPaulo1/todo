966d526caafb3e946b8a3aa36dee16921610566f
LuisPaulo1 <csluispaulo@gmail.com>
2023-07-14 18:21:27 -0300

    Funcionalidade Badge

--- frontend/src/app/components/read-all/read-all.component.ts
+++ frontend/src/app/components/read-all/read-all.component.ts
@@ -8,7 +8,9 @@ import { TodoService } from "src/app/services/todo.service";
   styleUrls: ["./read-all.component.css"],
 })
 export class ReadAllComponent implements OnInit {
+  closed = 0;
   list: Todo[] = [];
+  listFinished: Todo[] = [];
 
   constructor(private service: TodoService) {}
 
@@ -18,7 +20,14 @@ export class ReadAllComponent implements OnInit {
 
   findAll(): void {
     this.service.findAll().subscribe((resposta) => {
-      this.list = resposta;
+      resposta.forEach((todo) => {
+        if (todo.finalizado) {
+          this.listFinished.push(todo);
+        } else {
+          this.list.push(todo);
+        }
+      });
+      this.closed = this.listFinished.length;
     });
   }
 }
